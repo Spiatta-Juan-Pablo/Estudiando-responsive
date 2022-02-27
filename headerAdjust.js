bb5a637acc5c10d0cf00a4aa5faba89bc2daceeb
Juan Pablo Spiatta <spiattajuanpablo@outlook.es>
2022-02-27 17:24:04 -0300

    cambios

--- headerAdjust.js
+++ headerAdjust.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function(){
 
     var div = document.querySelector(".div-header");
-    var nav = document.querySelector(".contenedor-nav-header")
+    var nav = document.querySelector("#contenedor-nav-header-menu")
     div.classList.toggle("scroll", window.scrollY>0);
     nav.classList.toggle("scroll", window.scrollY>0)
 
